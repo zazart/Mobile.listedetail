1c5085a4495459643e0431ddca73b5e89d02c5bf
zazart <153142376+zazart@users.noreply.github.com>
2026-09-15 00:22:47 +0300

    TODO

--- app/src/main/java/mg/itu/listedetail/MainActivity.kt
+++ app/src/main/java/mg/itu/listedetail/MainActivity.kt
@@ -82,26 +82,28 @@ fun AppNavigation() {
                 onProduitClick = { produitId ->
                     // TODO 2 : naviguer vers le détail du produit cliqué.
                     // Une ligne :  navController.navigate("detail/$produitId")
+                    navController.navigate("detail/$produitId")
                 }
             )
         }
 
         // TODO 1 : déclarer la route du détail, avec son argument produitId.
         // Modèle :
-        //   composable("detail/{produitId}") { backStackEntry ->
-        //       val id = backStackEntry.arguments
-        //           ?.getString("produitId")?.toIntOrNull()
-        //       val produit = produits.find { it.id == id }
-        //       if (produit != null) {
-        //           EcranDetail(
-        //               produit = produit,
-        //               onRetour = {
-        //                   // TODO 3 : revenir à la liste (dépiler).
-        //                   // Une ligne :  navController.popBackStack()
-        //               }
-        //           )
-        //       }
-        //   }
+           composable("detail/{produitId}") { backStackEntry ->
+               val id = backStackEntry.arguments
+                   ?.getString("produitId")?.toIntOrNull()
+               val produit = produits.find { it.id == id }
+               if (produit != null) {
+                   EcranDetail(
+                       produit = produit,
+                       onRetour = {
+                           // TODO 3 : revenir à la liste (dépiler).
+                           // Une ligne :  navController.popBackStack()
+                           navController.popBackStack()
+                       }
+                   )
+               }
+           }
     }
 }
 
